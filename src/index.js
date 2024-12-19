14c51504b6b22288f43d6a55829ecb4d6f612f0a
Iggox <nachoariel2000@gmail.com>
2024-12-20 00:43:39 +0100

    Creación primera ruta get del servidor

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ const app = express();
 app.set("port", process.env.PORT || 8181);
 //al usar el puerto 8080 la terminal me indicaba que ya estaba en uso, por tanto usé 8181
 
+app.listen(app.get("port"), leerPuerto());
+
 function leerPuerto() {
   console.log("hola desde el puerto " + app.get("port"));
 }
 
-app.listen(app.get("port"), leerPuerto());
+app.get("/rutaGet", function (req, res) {
+  console.log("Hola desde la consola, alguien a accecido a la rutaGet");
+  res.send("Hola, has accedido a la rutaGet");
+});
